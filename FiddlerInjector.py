d92696fef09744110f50404e851e4355b572450c
kasselTrankos <alvaro.touzon@gmail.com>
2016-11-04 15:21:23 +0100

    stable version

--- FiddlerInjector.py
+++ FiddlerInjector.py
@@ -1,5 +1,10 @@
 import sublime, sublime_plugin, os, locale, sys, re
 from xml.dom import minidom
+from xml.etree import ElementTree
+import ntpath
+import subprocess
+from subprocess import Popen, PIPE, STDOUT
+from sys import version_info
 
 def get_setting(key, default=None):
 	settings = sublime.load_settings('FiddlerInjector.sublime-settings')
@@ -17,23 +22,26 @@ class NotFoundError(Exception):
 
 class FiddlerInjectorCommand():
 
-	def get_path(self, paths):
-		if paths:
-			return paths[0]
-		# DEV: On ST3, there is always an active view.
-		#   Be sure to check that it's a file with a path (not temporary view)
-		elif self.window.active_view() and self.window.active_view().file_name():
-			return self.window.active_view().file_name()
-		elif self.window.folders():
-			return self.window.folders()[0]
-		else:
-			sublime.error_message('Terminal: No place to open terminal to')
-			return False
+	def getCnt_cgt_info(self, path):
+		parts = os.path.normpath(path)
+		paths = parts.split(os.sep)
+		isCGT = False
+		name = ''
+		for path_ in paths:
+			if re.match('^cgt-', path_, flags=0) :
+				isCGT = True
+				name = path_.split('-', 1)[1]
+			if re.match('^cnt-', path_, flags=0):
+				name = path_.split('-', 2)[1]
+
+		return [isCGT, name[0].lower() + name[1:]]
+
+
 	def appendFiles(self, arr, path):
 		for content in os.listdir(path):
 			if(self.validateFiles(content)):
 				arr.append(content)
-
+		sublime.message_dialog('<::::::FILES IS::::>'.join(arr)+'----')
 	def validateFiles(self, file):
 		settings = sublime.load_settings('FiddlerInjector.sublime-settings')
 
@@ -43,12 +51,47 @@ class FiddlerInjectorCommand():
 			return True
 
 		return False
-	def generateXMLstr(self, files):
-		doc = minidom.Document()
-		root = doc.createElement('root')
-		doc.appendChild(root)
+	def get_file_name(self, path):
+		head, tail = ntpath.split(path)
+		return tail or ntpath.basename(head)
 
-	def create_fiddler(self, path,  parameters):
+	def generateXMLstr(self, files, cntName, isCGT, cgtName, path):
+		settings = sublime.load_settings('FiddlerInjector.sublime-settings')
+
+		doc = minidom.Document()
+		doc.toprettyxml(encoding="utf-8")
+		root = doc.createElement('State')
+		root.setAttribute('Enabled', 'true')
+		root.setAttribute('Fallthrough', 'true')
+		root.setAttribute('UseLatency', 'true')
+		doc.appendChild(root)
+		for file in files:
+			regexMatch = []
+			origPath = []
+			origPath.append(path)
+			origPath.append('\\')
+			origPath.append(file)
+			regexMatch.append('regex:(?inx)http:')
+			regexMatch.append(settings.get('DOMAIN'))
+			regexMatch.append(':')
+			regexMatch.append(settings.get('PORT'))
+			regexMatch.append('/')
+			regexMatch.append(cntName)
+			regexMatch.append('/')
+			if(isCGT):
+				regexMatch.append('cgt')
+				regexMatch.append('|')
+				regexMatch.append(cgtName)
+			regexMatch.append('/')
+			regexMatch.append(self.get_file_name(file))
+			tempChild = doc.createElement('ResponseRule')
+			tempChild.setAttribute('Match', ''.join(regexMatch))
+			tempChild.setAttribute('Action', ''.join(origPath))
+			tempChild.setAttribute('Enabled', 'true')
+			root.appendChild(tempChild)
+		sublime.message_dialog('<::::::jol::::>'+doc.toxml()+'----')
+		return doc
+	def create_fiddler(self, path,  parameters, cntName, isCGT, cgtName):
 
 		try:
 			if not path:
@@ -60,24 +103,55 @@ class FiddlerInjectorCommand():
 			args = []
 			files = []
 			settings = sublime.load_settings('FiddlerInjector.sublime-settings')
+
 			if os.path.isfile(path):
 				files.append(path)
 			else:
 				self.appendFiles(files, path)
 
 			args.extend(parameters)
-			ignore = settings.get('files')
-			sublime.message_dialog('<::::::jol::::>'.join(files))
-			self.generateXMLstr(files)
+			sublime.message_dialog('<::::::jol::::>'.join(files)+'----')
+			xml = self.generateXMLstr(files, cntName, isCGT, cgtName, path)
+			nameXML = []
+			nameXML.append(settings.get('saveOn'))
+			nameXML.append('\\')
+			if(isCGT):
+				nameXML.append('cgt-')
+				nameXML.append(cgtName)
+			else:
+				nameXML.append('cnt-')
+				nameXML.append(cntName)
+			nameXML.append('.farx')
+			xml.writexml(open(''.join(nameXML), 'w', encoding='utf8'),
+               encoding='utf-8',
+               newl='\n')
 		except (Exception) as exception:
 			sublime.error_message('FiddlerInjector: ' + str(exception))
 
 class FileFiddlerInjectorCommand(sublime_plugin.WindowCommand, FiddlerInjectorCommand):
 	def run(self, paths=[], parameters=None):
-		path = self.get_path(paths)
-		if not path:
-			return
-
+		sublime.message_dialog('<::::::jol::::>'+paths[0]+'----')
+		isCGT, name = self.getCnt_cgt_info(paths[0])
+		fname = self.window.active_view().file_name()
+		if fname == None:
+			fname = ""
+		if isCGT:
+			sublime.message_dialog('<::::::jol::::>CGT----')
+		else:
+			sublime.message_dialog('<::::::jol::::>CNT----')
+		def done(cntName):
+				self.create_fiddler(paths[0], parameters, cntName, isCGT, name)
 		if parameters is None:
 			parameters = get_setting('parameters', [])
-		self.create_fiddler(path, parameters)
+
+		if(isCGT):
+			self.window.show_input_panel(
+            "Name of CNT is: ", fname, done, None, None)
+		else:
+			self.create_fiddler(paths[0], parameters, name, isCGT, name)
+
+
+
+
+
+
